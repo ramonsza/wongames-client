e896b693c0a6d983a6a83a2bb590b582e9959259
Ramon Souza <ramon291997@gmail.com>
2023-11-04 17:43:56 -0300

    Create a Logo

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Logo from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo
+} as Meta
+
+type Story = StoryObj<typeof Logo>
+
+export const Default: Story = {
+  args: {
+    color: 'black'
+  }
+}
