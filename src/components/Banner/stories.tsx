2fad4f793ba2384ca477997f32f3937ff9f32cf1
Ramon Souza <ramon291997@gmail.com>
2023-11-12 21:18:36 -0300

    Initiate Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner
+} as Meta
+
+type Story = StoryObj<typeof Banner>
+
+export const Default: Story = {
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x500',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+}
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,24 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+import Banner from '.'
+
+const args = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x500',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...args} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+  })
+})
