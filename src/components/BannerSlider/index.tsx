02f64a0d1264e37d1f137646e11a7d236e5101fd
Ramon Souza <ramon291997@gmail.com>
2023-11-19 11:48:52 -0300

    Create BannerSlider component

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,38 @@
+import Banner, { BannerProps } from '@/components/Banner'
+import Slider from '@/components/Slider'
+
+import * as S from './styles'
+import { Settings } from 'react-slick'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: Settings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default BannerSlider
---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react'
+import BannerSlider from '.'
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x581',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+type Story = StoryObj<typeof BannerSlider>
+
+export const Default: Story = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <BannerSlider {...args} />
+    </div>
+  ),
+  args: {
+    items
+  }
+}
