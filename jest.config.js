5d493969e6162ec04e92a5a11f3b1f28210910b1
Ramon Souza <ramon291997@gmail.com>
2023-11-20 09:09:07 -0300

    Create Container component

--- jest.config.js
+++ jest.config.js
@@ -22,5 +22,6 @@ module.exports = {
     '^styled-components':
       'styled-components/dist/styled-components.browser.cjs.js',
     '@/(.*)': '<rootDir>/src/$1'
-  }
+  },
+  prettierPath: null
 }
