359fd71c341e1369e1a70b5833b98eedd9799630
Ramon Souza <ramon291997@gmail.com>
2023-11-23 21:07:34 -0300

    Create Checked component and intial styles

---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Checkbox from '.'
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox
+} as Meta
+
+type Story = StoryObj<typeof Checkbox>
+
+export const Default: Story = {}
---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+
+import Checkbox from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+
+describe('<Checkbox />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517'
+    })
+  })
+})
