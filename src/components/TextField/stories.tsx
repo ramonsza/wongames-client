56b79b50f26a8aea6953ec1f1ebf8a16e2076f01
Ramon Souza <ramon291997@gmail.com>
2023-11-25 18:18:16 -0300

    Create TextField component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TextField from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' }
+  }
+} as Meta
+
+export const Default: StoryObj = {}
---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,59 @@
+import { screen, waitFor } from '@testing-library/react'
+
+import TextField from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
+
+describe('<TextField />', () => {
+  it('Renders with Label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('Renders without Label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('Renders with placeholder', () => {
+    renderWithTheme(<TextField placeholder="hey you" />)
+
+    expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
+  })
+
+  it('Changes its value when typing', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it('Is accessible by tab', async () => {
+    renderWithTheme(
+      <TextField label="TextField" labelFor="TextField" id="TextField" />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    await userEvent.tab()
+    expect(input).toHaveFocus()
+  })
+})
